d0ac166496a75bffcdec2af11f6e5fce99d60478
Jonathan E. Moffie <jonathanmoffie.sinow@gmail.com>
2018-03-09 13:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return 'Happy Independence Day, you!'
 }
+
+function holidayCountdown(holiday, days){
+  return 'It\'s 20 days until Mother\'s Day!'
+}
